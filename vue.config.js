afc3e6987894b87ab1e04f1e828e07dd35e2f716
Genndiy <gsabinin@icloud.com>
2022-04-12 09:58:38 +0300

    add some config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "build",
+  devServer: {
+    proxy: "http://localhost:8000",
+  },
 });
