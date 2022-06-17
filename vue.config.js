e5ba3b286515494ac7150902ad7b3b60899c297d
Genndiy <gsabinin@icloud.com>
2022-06-17 15:08:35 +0300

    rename outputDir

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
 
-  outputDir: "build",
+  outputDir: "docs",
 
   devServer: {
     proxy: "http://151.248.114.103/",
