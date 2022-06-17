4e158239ac07d5b73fdfb8e362ce8a5ddd25c02d
Genndiy <gsabinin@icloud.com>
2022-06-17 15:22:57 +0300

    add deploy file

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
 
+  publicPath: process.env.NODE_ENV === "production" ? "/study-vue/" : "/",
+
   outputDir: "docs",
 
   devServer: {
