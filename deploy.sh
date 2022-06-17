c36e52078f5c084f0a8c384081cf6dd07ec6c33e
Genndiy <gsabinin@icloud.com>
2022-06-17 15:38:39 +0300

    fixed publicPath in vue config

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gesha50/study-vue.git master:gh-pages
+git push -f git@github.com:gesha50/study-vue.git main:gh-pages
 
 cd -
