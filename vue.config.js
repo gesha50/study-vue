931bb5df4bba5557cf55fce9b51e2fa173196e4e
Genndiy <gsabinin@icloud.com>
2022-06-17 15:27:49 +0300

    fixed publicPath in vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
 
-  publicPath: process.env.NODE_ENV === "production" ? "/study-vue/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/docs/" : "/",
 
   outputDir: "docs",
 
